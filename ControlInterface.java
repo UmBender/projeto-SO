c91cfd0e08fa269c212bbde9a24bd6b33e10b250
Gustavo Bender <gustavo.benderr@gmail.com>
2024-05-28 14:58:49 -0300

    feat: Adicionei o template basico para o projeto.

---
+++ ControlInterface.java
@@ -0,0 +1,7 @@
+public interface ControlInterface {
+	public void startSimulation();
+	public void suspendSimulation();
+	public void resumeSimulation();
+	public void stopSimulation();
+	public void displayProcessQueues();
+}
