1abc08f38c77643ba2dfcfc3f1d4bd99760dcfd4
Caio Uehara <caioueharamartins@gmail.com>
2024-06-09 19:32:49 -0300

    Mudar a estrutura dos arquivos para o projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import UserInterface.UserInterface;
 
-import javax.swing.SwingUtilities;
+import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) {
 		//Schedule a job for the event-dispatching thread:
 		//creating and showing this application's GUI.
 		javax.swing.SwingUtilities.invokeLater(new UserInterface());
+
+		BnfParser parser = new BnfParser("Examples/Simple-Math.bnf");
+		ParseNode node1 = parser.parse("3+4");
+		ParseNode node2 = parser.parse("(10 + (3 - 4))");
 	}
+
 }
